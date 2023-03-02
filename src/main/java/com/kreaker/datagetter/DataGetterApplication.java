5485745838d25fbbeecc34865aa204a7e29612a3
Alex / Kreaker <alexlm78@users.noreply.github.com>
2023-03-02 00:16:25 -0600

    feat(prg): structure base for app.

---
+++ src/main/java/com/kreaker/datagetter/DataGetterApplication.java
@@ -0,0 +1,13 @@
+package com.kreaker.datagetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataGetterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kreaker/datagetter/DataGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreaker.datagetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
